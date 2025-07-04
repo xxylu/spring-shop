c862e1243ea735bdd7905f2e03e9cf5c15edb57b
xxylu <oliwierpachulski@gmail.com>
2025-07-04 16:40:44 +0200

    Database connection manager

---
+++ src/main/java/org/example/database/DatabaseConnection.java
@@ -0,0 +1,33 @@
+package org.example.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private static DatabaseConnection instance;
+    private final String url;
+
+    public static DatabaseConnection getInstance() {
+        if (instance == null) {
+            instance = new DatabaseConnection();
+        }
+        return instance;
+    }
+
+    private DatabaseConnection() {
+        url = System.getenv("DB_URL");
+        if (url == null) {
+            throw new RuntimeException("DB_URL not set!");
+        }
+    }
+    public Connection getConnection() {
+        try {
+            return DriverManager.getConnection(url);
+        } catch (SQLException e) {
+            System.out.print("Błąd połaczenia z Bazą Danych");
+            System.exit(0);
+            throw new RuntimeException(e);
+        }
+    }
+}
