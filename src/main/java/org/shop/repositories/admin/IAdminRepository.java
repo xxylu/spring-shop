38f72c3a80aab91928551c73bce23b8d7dd69cc1
xxylu <oliwierpachulski@gmail.com>
2025-07-08 20:44:10 +0200

    fix

--- src/main/java/org/shop/repositories/admin/IAdminRepository.java
+++
@@ -1,8 +0,0 @@
-package org.shop.repositories.admin;
-
-public interface IAdminRepository {
-    public void DeleteUser(String UserId);
-    public void BanUser(String userId);
-    public void DeleteOrder(String orderId);
-    public void PromoteUserToAdmin(String userId);
-}
