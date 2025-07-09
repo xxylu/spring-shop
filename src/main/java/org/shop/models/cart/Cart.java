2baba85ae0464bedbae6a97111a8020dceaea082
xxylu <oliwierpachulski@gmail.com>
2025-07-09 13:16:09 +0200

    fix

--- src/main/java/org/shop/models/cart/Cart.java
+++ src/main/java/org/shop/models/cart/Cart.java
@@ -1,9 +1,15 @@
 package org.shop.models.cart;
 
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import lombok.*;
+
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import org.shop.models.product.Product;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Data
 @NoArgsConstructor
@@ -16,12 +22,25 @@ public class Cart {
 
 
     public void convertToJson(List<Product> products) {
-        String json = "";
-
-        for (Product product : products) {
-
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            this.products = objectMapper.writeValueAsString(products);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
         }
+    }
 
-        this.products = json;
+    public List<Product> convertFromJson() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            List<Product> productList = objectMapper.readValue(
+                    this.products,
+                    new TypeReference<List<Product>>() {}
+            );
+            return productList;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return new ArrayList<>();
+        }
     }
 }
