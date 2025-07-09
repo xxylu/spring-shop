d2d32a878aa73540dbeb8a0bcb0ffd8cdda3a1f9
xxylu <oliwierpachulski@gmail.com>
2025-07-09 13:16:21 +0200

    fix

--- src/main/java/org/shop/models/order/Order.java
+++ src/main/java/org/shop/models/order/Order.java
@@ -1,6 +1,14 @@
 package org.shop.models.order;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.*;
+import org.shop.models.product.Product;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 @NoArgsConstructor
@@ -11,4 +19,28 @@ public class Order {
     private String userId;
     private OrderStatus status;
     private String products;
+
+
+    public void convertToJson(List<Product> products) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            this.products = objectMapper.writeValueAsString(products);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public List<Product> convertFromJson() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            List<Product> productList = objectMapper.readValue(
+                    this.products,
+                    new TypeReference<List<Product>>() {}
+            );
+            return productList;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return new ArrayList<>();
+        }
+    }
 }
