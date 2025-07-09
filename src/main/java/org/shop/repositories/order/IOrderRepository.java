51ffa4588f384411f0619a7602ddb9d0cc4f5c66
xxylu <oliwierpachulski@gmail.com>
2025-07-09 13:16:43 +0200

    new methods

--- src/main/java/org/shop/repositories/order/IOrderRepository.java
+++ src/main/java/org/shop/repositories/order/IOrderRepository.java
@@ -1,5 +1,14 @@
 package org.shop.repositories.order;
 
+import org.shop.models.order.Order;
+
+import java.util.Optional;
+
 public interface IOrderRepository {
+    Optional<Order> findById(String id);
+    void updateOrder(Order order);
+    Optional<Order> findByUsername(String username);
+    void addOrder(Order order);
+    void deleteOrder(String orderId);
 
 }
--- src/main/java/org/shop/repositories/order/OrderRepository.java
+++ src/main/java/org/shop/repositories/order/OrderRepository.java
@@ -1,5 +1,33 @@
 package org.shop.repositories.order;
 
+import org.shop.models.order.Order;
+
+import java.util.Optional;
+
 public class OrderRepository implements IOrderRepository {
 
+    @Override
+    public Optional<Order> findById(String id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public void updateOrder(Order order) {
+
+    }
+
+    @Override
+    public Optional<Order> findByUsername(String username) {
+        return Optional.empty();
+    }
+
+    @Override
+    public void addOrder(Order order) {
+
+    }
+
+    @Override
+    public void deleteOrder(String orderId) {
+
+    }
 }
